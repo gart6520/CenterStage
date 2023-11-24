e3c11af9e8d1c76ef15a96423358e7123178c387
DELL <khiem06072007@gmail.com>
2023-11-24 14:33:11 +0700

    fix april tag pose estimator formula

--- Team24751/src/main/java/org/firstinspires/ftc/team24751/subsystems/PoseEstimatorCamera.java
+++ Team24751/src/main/java/org/firstinspires/ftc/team24751/subsystems/PoseEstimatorCamera.java
@@ -7,6 +7,7 @@ import com.sun.tools.javac.util.Pair;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.ExposureControl;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.controls.GainControl;
+
 import com.acmerobotics.roadrunner.Vector2d;
 
 import org.firstinspires.ftc.robotcore.external.matrices.VectorF;
@@ -20,6 +21,8 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 import static org.firstinspires.ftc.team24751.Constants.SENSITIVITY.*;
 import static org.firstinspires.ftc.team24751.Constants.*;
+
+import android.util.Size;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,7 +41,7 @@ public class PoseEstimatorCamera {
     }
 
     //Null if no result or result too unreliable
-    public Vector2d cameraLoop() {
+    public Vector2d cameraLoop(double botAngle) {
         List<AprilTagDetection> currentDetections = aprilTag.getDetections();
         if (currentDetections.isEmpty()) return null;
         float decisionMarginSum = 0;
@@ -46,7 +49,7 @@ public class PoseEstimatorCamera {
         for (AprilTagDetection detection : currentDetections) {
             if (detection.metadata == null) continue;
             decisionMarginSum += detection.decisionMargin;
-            robotPoseResult.add(new Pair<>(getCameraPoseFromApriltagDetection(detection), detection.decisionMargin));
+            robotPoseResult.add(new Pair<>(getCameraPoseFromApriltagDetection(detection, botAngle), detection.decisionMargin));
         }
         if (decisionMarginSum < MARGIN_DECISION_THRESHOLD) return null;
         Vector2d currentPose = new Vector2d(0, 0);
@@ -56,18 +59,27 @@ public class PoseEstimatorCamera {
         return currentPose;
     }
 
-    private Vector2d getCameraPoseFromApriltagDetection(AprilTagDetection detection) {
+    private Vector2d getCameraPoseFromApriltagDetection(AprilTagDetection detection, double botAngle) {
         VectorF _pos = detection.metadata.fieldPosition;
         Vector2d pos = new Vector2d(_pos.get(0), _pos.get(1));
         Vector2d aprilTagPose = new Vector2d(pos.x, pos.y);
-        float conversionFactor = detection.metadata.distanceUnit == DistanceUnit.METER? (float)M_TO_INCH : 1;
+        float conversionFactor = detection.metadata.distanceUnit == DistanceUnit.METER ? (float) M_TO_INCH : 1;
         Vector2d cameraToApriltag = new Vector2d((float) detection.ftcPose.x * conversionFactor, (float) detection.ftcPose.y * conversionFactor);
-        return aprilTagPose.minus(cameraToApriltag);
+        double radians = Math.toRadians(90 - botAngle);
+        double cos = Math.cos(radians);
+        double sin = Math.sin(radians);
+        Vector2d cameraToAprilTagWorld = new Vector2d(
+                cameraToApriltag.x * cos - cameraToApriltag.y * sin,
+                cameraToApriltag.x * sin + cameraToApriltag.y * cos
+        );
+        return aprilTagPose.minus(cameraToAprilTagWorld);
     }
 
-    public void initAprilTag(HardwareMap hardwareMap) {
+    public void initAprilTag() {
 
-        aprilTag = new AprilTagProcessor.Builder().setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary()).build();
+        aprilTag = new AprilTagProcessor.Builder()
+                .setTagLibrary(AprilTagGameDatabase.getCenterStageTagLibrary())
+                .build();
         // Adjust Image Decimation to trade-off detection-range for detection-rate.
         // eg: Some typical detection data using a Logitech C920 WebCam
         // Decimation = 1 ..  Detect 2" Tag from 10 feet away at 10 Frames per second
@@ -80,15 +92,18 @@ public class PoseEstimatorCamera {
         // Create the vision portal by using a builder.
 
         visionPortal = new VisionPortal.Builder()
-                .setCamera(hardwareMap.get(WebcamName.class, cameraName))
+                .setCamera(linearOpMode.hardwareMap.get(WebcamName.class, cameraName))
+                .setCameraResolution(new Size(640, 480))
                 .addProcessor(aprilTag)
+                .setStreamFormat(VisionPortal.StreamFormat.MJPEG)
                 .build();
     }
 
-    /** Remember to setManualExposure(6, 250); after init
+    /**
+     * Remember to setManualExposure(6, 250); after init
      *
-     * @param exposureMS exposure
-     * @param gain gain
+     * @param exposureMS lower -> less motion blue but darker image
+     * @param gain       make image brighter artificially
      */
     private void setManualExposure(int exposureMS, int gain) {
         // Wait for the camera to be open, then use the controls
