dd272e515574c39aa17758d076a1e1143a037488
DELL <khiem06072007@gmail.com>
2024-01-03 22:24:32 +0700

    odometry pods with roadrunner 0.5.5

---
+++ Team24751/src/main/java/org/firstinspires/ftc/team24751/opmodes/test/TestOdometryPod.java
@@ -0,0 +1,102 @@
+/*
+ * Main manual drive OpMode for GART 24751's FTC code
+ * Written by gvl610
+ * Date created: 9/11/2023
+ */
+
+package org.firstinspires.ftc.team24751.opmodes.test;
+
+// Import modules
+
+import static org.firstinspires.ftc.team24751.Constants.SPEED.DRIVEBASE_SPEED_X;
+import static org.firstinspires.ftc.team24751.Constants.SPEED.DRIVEBASE_SPEED_Y;
+import static org.firstinspires.ftc.team24751.Constants.SPEED.DRIVEBASE_SPEED_Z;
+
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.team24751.subsystems.Drivebase;
+import org.firstinspires.ftc.team24751.subsystems.Gyro;
+import org.firstinspires.ftc.team24751.subsystems.PoseStorage;
+import org.firstinspires.ftc.team24751.subsystems.StandardTrackingWheelLocalizer;
+import org.firstinspires.ftc.team24751.subsystems.old_roadrunner.geometry.Pose2d;
+
+import java.util.List;
+
+@TeleOp(name="Test Odo Pod", group="Test")
+public class TestOdometryPod extends LinearOpMode {
+    // Total run time
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode() {
+        // Update status
+        telemetry.addData("Status", "Initializing");
+        telemetry.update();
+
+        // Enable bulk reads in auto mode
+        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
+
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+
+        StandardTrackingWheelLocalizer localizer = new StandardTrackingWheelLocalizer(hardwareMap);
+        // Wait for the driver to press PLAY
+        waitForStart();
+
+        // Init gyro
+        Gyro gyro = new Gyro(this);
+        gyro.init();
+
+        // Init drivebase
+        Drivebase drivebase = new Drivebase(this, gyro);
+        drivebase.init();
+
+        // Load last pose from auto mode
+        drivebase.setCurrentPose(PoseStorage.getPose());
+
+        // Update status
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Reset runtime
+        runtime.reset();
+
+        // Loop, run until driver presses STOP
+        while (opModeIsActive()) {
+            // Control drivebase manually
+            // Get speed
+
+            double speed = gamepad1.right_trigger > 0.15 ? 1 : 0.5;
+
+            // Get joystick axis values
+            // Left joystick is used for driving bot in up/down/left/right direction, while right joystick is used for rotating the bot
+            double left_y = -gamepad1.left_stick_y * DRIVEBASE_SPEED_Y * speed; // Y axis is inverted
+            double left_x = gamepad1.left_stick_x * DRIVEBASE_SPEED_X * speed;
+            double right_x = gamepad1.right_stick_x * DRIVEBASE_SPEED_Z * speed;
+
+            // Drive
+            // drivebase.drive(left_x, left_y, right_x); // Drive bot-oriented
+            drivebase.driveFieldOriented(left_x, left_y, right_x); // Drive field-oriented
+
+            // TODO: Implement buttons for mechanisms and semi-auto drive
+            // IMPORTANT NOTE: For semi-auto buttons, encoder MUST be reset
+            // RUN_USING_ENCODER mode (using drivebase.resetRunUsingEncoder())
+            // before following any trajectory, and MUST be reset back to
+            // RUN_WITHOUT_ENCODER mode (using drivebase.resetRunWithoutEncoder())
+            // after finishing the trajectory
+
+            // Show elapsed run time
+            telemetry.addData("Yaw", gyro.getYawDeg());
+
+            localizer.update();
+            Pose2d pose = localizer.getPoseEstimate();
+            telemetry.addData("X, Y, theta", pose.toString());
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+}
