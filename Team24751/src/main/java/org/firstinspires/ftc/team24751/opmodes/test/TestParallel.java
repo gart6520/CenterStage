caecaf2228484f49f26845fba25a191f6cd44c07
mynameiskhanhhh <91131575+mynameiskhanhhh@users.noreply.github.com>
2024-01-09 23:13:42 +0700

    Added Claw Subsystem

--- Team24751/src/main/java/org/firstinspires/ftc/team24751/opmodes/test/TestParallel.java
+++ Team24751/src/main/java/org/firstinspires/ftc/team24751/opmodes/test/TestParallel.java
@@ -1,33 +1,30 @@
 package org.firstinspires.ftc.team24751.opmodes.test;
 
-import static org.firstinspires.ftc.team24751.Constants.HARDWARE_CONSTANT.INITAL_WRIST_SERVO_ANGLE;
-import static org.firstinspires.ftc.team24751.Constants.HARDWARE_CONSTANT.Arm.MOTOR_POSITION_AT_UPWARD_VERTICAL;
-import static org.firstinspires.ftc.team24751.Constants.HARDWARE_CONSTANT.Arm.MOTOR_POSITION_AT_FRONT_HORIZONTAL;
-import static org.firstinspires.ftc.team24751.Constants.HARDWARE_CONSTANT.REV_SERVO_ANGLE_RANGE;
+import static org.firstinspires.ftc.team24751.Constants.HARDWARE_CONSTANT.MOTOR_POSITION_AT_PERPENDICULAR;
+import static org.firstinspires.ftc.team24751.Constants.HARDWARE_CONSTANT.MOTOR_POSITION_AT_ZERO;
 import static org.firstinspires.ftc.team24751.Constants.HARDWARE_CONSTANT.REV_SERVO_PWM_RANGE;
-//import static org.firstinspires.ftc.team24751.Constants.HARDWARE_CONSTANT.SERVO_POSITION_AT_PERPENDICULAR;
-//import static org.firstinspires.ftc.team24751.Constants.HARDWARE_CONSTANT.SERVO_POSITION_AT_ZERO;
+import static org.firstinspires.ftc.team24751.Constants.HARDWARE_CONSTANT.SERVO_POSITION_AT_HORIZONTAL_BACK;
+import static org.firstinspires.ftc.team24751.Constants.HARDWARE_CONSTANT.SERVO_POSITION_AT_ZERO;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
-
-import org.firstinspires.ftc.team24751.subsystems.AngleServo;
+import com.qualcomm.robotcore.hardware.ServoImplEx;
 
 @TeleOp(name = "Test Parallel", group = "Test")
 public class TestParallel extends LinearOpMode {
     private DcMotor leftArmMotor = null;
     private DcMotor rightArmMotor = null;
     private DcMotor elevatorMotor = null;
-    private AngleServo wristServo = null;
+    private ServoImplEx wristServo = null;
 
     private double getMotorAngleDeg() {
-        double a = (MOTOR_POSITION_AT_UPWARD_VERTICAL - MOTOR_POSITION_AT_FRONT_HORIZONTAL) / 90.0;
-        double b = MOTOR_POSITION_AT_FRONT_HORIZONTAL;
-        double currentPosition = leftArmMotor.getCurrentPosition();
-        return a * currentPosition + b;
+        return 90.0 / (MOTOR_POSITION_AT_PERPENDICULAR - MOTOR_POSITION_AT_ZERO) * leftArmMotor.getCurrentPosition() - 90.0 / (MOTOR_POSITION_AT_PERPENDICULAR - MOTOR_POSITION_AT_ZERO) * MOTOR_POSITION_AT_ZERO;
+    }
+
+    private void setWristAngleDeg(double deg) {
+        wristServo.setPosition(deg * (180.0 / SERVO_POSITION_AT_HORIZONTAL_BACK) / 180.0);
     }
 
     @Override
@@ -35,6 +32,7 @@ public class TestParallel extends LinearOpMode {
         leftArmMotor = hardwareMap.get(DcMotor.class, "leftArmMotor");
         rightArmMotor = hardwareMap.get(DcMotor.class, "rightArmMotor");
         elevatorMotor = hardwareMap.get(DcMotor.class, "elevatorMotor");
+        wristServo = (ServoImplEx) hardwareMap.get(ServoImplEx.class, "wristServo");
 
         leftArmMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightArmMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -46,34 +44,35 @@ public class TestParallel extends LinearOpMode {
         rightArmMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         rightArmMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-        wristServo = new AngleServo("wristServo", INITAL_WRIST_SERVO_ANGLE, REV_SERVO_ANGLE_RANGE, this);
-        wristServo.init(REV_SERVO_PWM_RANGE);
-        wristServo.getServo().setDirection(Servo.Direction.REVERSE);
-        wristServo.getServo().scaleRange(0.1, 0.9);
+        wristServo.setPwmRange(REV_SERVO_PWM_RANGE);
 
         waitForStart();
 
         while (opModeIsActive()) {
-            wristServo.setAngle(120.0 + getMotorAngleDeg());
+            if (getMotorAngleDeg() >= 45){
+                //setWristAngleDeg(60.0 + getMotorAngleDeg());
+                //setWristAngleDeg(300.0 - getMotorAngleDeg());
+            }
             if (gamepad1.triangle) {
-                leftArmMotor.setPower(0.3);
-                rightArmMotor.setPower(0.3);
+                leftArmMotor.setPower(0.5);
+                rightArmMotor.setPower(0.5);
             } else if (gamepad1.cross) {
-                leftArmMotor.setPower(-0.1);
-                rightArmMotor.setPower(-0.1);
+                leftArmMotor.setPower(-0.5);
+                rightArmMotor.setPower(-0.5);
             } else {
                 leftArmMotor.setPower(0);
                 rightArmMotor.setPower(0);
             }
 
             if (gamepad1.circle) {
-                elevatorMotor.setPower(0.8);
+                elevatorMotor.setPower(0.6);
             } else if (gamepad1.square) {
-                elevatorMotor.setPower(-0.8);
+                elevatorMotor.setPower(-0.6);
             } else {
                 elevatorMotor.setPower(0);
             }
-            telemetry.addData("Current Position", leftArmMotor.getCurrentPosition());
+            telemetry.addData("Current Motor Degree", getMotorAngleDeg());
+            //telemetry.addData("Current Servo Degree", wristServo.getAngle());
             telemetry.update();
         }
     }
