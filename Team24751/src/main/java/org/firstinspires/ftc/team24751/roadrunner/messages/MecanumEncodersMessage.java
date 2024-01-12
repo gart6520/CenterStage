012f25378bf922fd61a1688dc15ebb100d68473b
DELL <khiem06072007@gmail.com>
2024-01-12 11:46:20 +0700

    Idk

---
+++ Team24751/src/main/java/org/firstinspires/ftc/team24751/roadrunner/messages/MecanumEncodersMessage.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.team24751.roadrunner.messages;
+
+import com.acmerobotics.roadrunner.ftc.PositionVelocityPair;
+
+public final class MecanumEncodersMessage {
+    public long timestamp;
+    public PositionVelocityPair leftFront;
+    public PositionVelocityPair leftBack;
+    public PositionVelocityPair rightBack;
+    public PositionVelocityPair rightFront;
+
+    public MecanumEncodersMessage(PositionVelocityPair leftFront, PositionVelocityPair leftBack, PositionVelocityPair rightBack, PositionVelocityPair rightFront) {
+        this.timestamp = System.nanoTime();
+        this.leftFront = leftFront;
+        this.leftBack = leftBack;
+        this.rightBack = rightBack;
+        this.rightFront = rightFront;
+    }
+}
