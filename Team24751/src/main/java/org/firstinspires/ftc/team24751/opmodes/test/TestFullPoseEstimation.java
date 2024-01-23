bf0a4dfb7600536cb8eb06e228ce269479c214fd
DELL <khiem06072007@gmail.com>
2024-01-23 13:45:24 +0700

    Pose fusion

---
+++ Team24751/src/main/java/org/firstinspires/ftc/team24751/opmodes/test/TestFullPoseEstimation.java
@@ -0,0 +1,113 @@
+/*
+ * Main manual drive OpMode for GART 24751's FTC code
+ * Written by gvl610
+ * Date created: 9/11/2023
+ */
+
+package org.firstinspires.ftc.team24751.opmodes.test;
+
+// Import modules
+
+import static org.firstinspires.ftc.team24751.Constants.DEVICES.*;
+import static org.firstinspires.ftc.team24751.Constants.SPEED.*;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.hardware.lynx.LynxModule;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.team24751.subsystems.FuseSensor;
+import org.firstinspires.ftc.team24751.subsystems.PoseStorage;
+import org.firstinspires.ftc.team24751.subsystems.drivebase.Drivebase;
+import org.firstinspires.ftc.team24751.subsystems.vision.Camera;
+import org.firstinspires.ftc.team24751.subsystems.vision.PoseEstimatorAprilTagProcessor;
+
+import java.util.List;
+
+@TeleOp(name = "Test Full Pose Estimation", group = "Test")
+public class TestFullPoseEstimation extends LinearOpMode {
+    // Total run time
+    private ElapsedTime runtime = new ElapsedTime();
+
+    // Subsystems
+    private Drivebase drivebase = null;
+    private Camera fieldCamera = new Camera(FIELD_CAMERA_NAME, this);
+    private PoseEstimatorAprilTagProcessor aprilTag = new PoseEstimatorAprilTagProcessor(fieldCamera, this);
+    private FuseSensor poseXFuse = new FuseSensor(0);
+    private FuseSensor poseYFuse = new FuseSensor(0);
+
+
+    @Override
+    public void runOpMode() {
+        // Update status
+        telemetry.addData("Status", "Initializing");
+        telemetry.update();
+
+        // Enable bulk reads in auto mode
+        List<LynxModule> allHubs = hardwareMap.getAll(LynxModule.class);
+
+        for (LynxModule hub : allHubs) {
+            hub.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        }
+
+        // Init drivebase
+        drivebase = new Drivebase(this);
+
+        // Load last pose from auto mode
+        drivebase.setPoseEstimate(PoseStorage.getPose());
+
+        // Update status
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the driver to press PLAY
+        waitForStart();
+
+        // Reset runtime
+        runtime.reset();
+
+        // Loop, run until driver presses STOP
+        while (opModeIsActive()) {
+            drivebase.update();
+
+            // Control drivebase manually
+            // Get speed
+
+            double speed = gamepad1.right_trigger > 0.15 ? 1 : 0.5;
+
+            // Get joystick axis values
+            // Left joystick is used for driving bot in up/down/left/right direction, while right joystick is used for rotating the bot
+            double left_y = -gamepad1.left_stick_y * DRIVEBASE_SPEED_Y * speed; // Y axis is inverted
+            double left_x = gamepad1.left_stick_x * DRIVEBASE_SPEED_X * speed;
+            double right_x = gamepad1.right_stick_x * DRIVEBASE_SPEED_Z * speed;
+
+            // Drive
+            // drivebase.drive(left_x, left_y, right_x); // Drive bot-oriented
+            drivebase.driveFieldOriented(left_x, left_y, right_x); // Drive field-oriented
+
+            // TODO: Implement buttons for mechanisms and semi-auto drive
+            // IMPORTANT NOTE: For semi-auto buttons, encoder MUST be reset
+            // RUN_USING_ENCODER mode (using drivebase.resetRunUsingEncoder())
+            // before following any trajectory, and MUST be reset back to
+            // RUN_WITHOUT_ENCODER mode (using drivebase.resetRunWithoutEncoder())
+            // after finishing the trajectory
+
+            // Show odoPose estimation
+            Pose2d odoPose = drivebase.getPoseEstimate();
+            Vector2d aprilTagPos = aprilTag.getCurrentPosFromAprilTag(odoPose.getHeading());
+            Double xAprilTag = aprilTagPos == null ? null : aprilTagPos.getX();
+            Double yAprilTag = aprilTagPos == null ? null : aprilTagPos.getY();
+            double x = poseXFuse.update(odoPose.getX(), xAprilTag);
+            double y = poseXFuse.update(odoPose.getY(), yAprilTag);
+
+            telemetry.addData("X", x);
+            telemetry.addData("Y", y);
+            telemetry.addData("Heading", odoPose.getHeading());
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+}
